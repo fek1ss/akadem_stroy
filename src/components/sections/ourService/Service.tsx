103fb77fb940b32bc49a93f8d9e0e6c20841ab0f
fek1ss <damirfrontend.dev@gmail.com>
2026-02-06 13:53:42 +0600

    services

--- src/components/sections/ourService/Service.tsx
+++ src/components/sections/ourService/Service.tsx
@@ -11,7 +11,7 @@ export function Service() {
   return (
     <motion.div
       className={styles.services}
-      initial={{ y: 200, opacity: 0 }}
+      initial={{ y: 100, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, ease: 'easeOut' }}
       viewport={{ once: false, amount: 0.2 }}  
